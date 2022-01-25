eacc62dd1d33f01bd290a5e507470220cf7c42d6
Charles Fernandes de Souza <souzach@outlook.com>
2022-01-25 19:50:35 -0300

    Chapter 2 - IDES, Variables, Conversions and Hello World

---
+++ Chapter-02-IDES-Variables-Conversions-Hello-World/Example01/src/org/souza/charles/HelloWorld.java
@@ -0,0 +1,14 @@
+package org.souza.charles;
+/**
+ * Course title: Java Fundamentos
+ * Example adapted by: Charles Fernandes de Souza
+ * Date: January 22, 2022
+ */
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		
+		System.out.print("Hello, World!");
+	}
+}
