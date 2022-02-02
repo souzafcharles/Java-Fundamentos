160f88c431a627772ba90a1b9f89543c2ce0906e
Charles Fernandes de Souza <souzach@outlook.com>
2022-02-01 23:29:34 -0300

    Chapter 5 - Repetition Statements, Arrays, Strings, and Collections - Version 06

---
+++ Chapter-05-RepetitionStatements-Arrays-Strings-Collections/src/org/souza/charles/collections/example/ExempleCollections.java
@@ -0,0 +1,71 @@
+package org.souza.charles.collections.example;
+/**
+ * Course title: Java Fundamentos
+ * Instructor: Fernando Lima - FIAP
+ * Example adapted by: Charles Fernandes de Souza
+ * Date: January 31, 2022
+ */
+import java.util.*;
+
+public class ExempleCollections {
+
+    public static void main(String[] args) {
+
+        ArrayList cart = new ArrayList();
+        Double value = 100.55;
+        int value2 = 1;
+        int value3;
+
+        cart.add(value);
+        cart.add("Grape");
+        cart.add(value2);
+
+        System.out.println(cart.get(1));
+
+        //cast
+        value3 = (int) cart.get(2);
+
+        System.out.println(value);
+
+        List<String> cart2 = new ArrayList<String>();
+
+        System.out.println(cart2.isEmpty()); //true
+
+        cart2.add("Apple");
+        cart2.add("Strawberry");
+        cart2.add("Apple");
+        cart2.set(1, "Pear");
+
+        System.out.println(cart2.isEmpty());
+        System.out.println(cart2.size());
+
+        System.out.println(cart2.contains("Apple"));
+        System.out.println(cart2.indexOf("Apple"));
+        System.out.println(cart2.get(cart2.indexOf("Apple")));
+        System.out.println(cart2.lastIndexOf("Apple"));
+
+        Set<String> basket = new HashSet<String>();
+
+
+        //HashSet does not allow repeated items
+        System.out.println(basket.isEmpty());
+        basket.add("Apple");
+        basket.add("Apple");
+        basket.add("apple");
+        System.out.println(basket.isEmpty());
+
+        System.out.println(basket.size());
+        System.out.println(basket);
+
+        Map<String, String> box = new HashMap<String, String>();
+        box.put("M2225", "Charles");
+        box.put("M2226", "Balthazar");
+        box.put("M2227", "Ophélia");
+
+        System.out.println(box.isEmpty());
+        System.out.println(box.size());
+        System.out.println(box.containsKey("M2225"));
+        System.out.println(box.containsValue("Charles"));
+        System.out.println(box);
+    }
+}
---
+++ Chapter-05-RepetitionStatements-Arrays-Strings-Collections/src/org/souza/charles/collections/exercise/ExerciseCollections.java
@@ -0,0 +1,89 @@
+package org.souza.charles.collections.exercise;
+/**
+ * Course title: Java Fundamentos
+ * Instructor: Fernando Lima - FIAP
+ * Example adapted by: Charles Fernandes de Souza
+ * Date: February 01, 2022
+ */
+
+import java.util.*;
+
+public class ExerciseCollections {
+
+    public static void main(String[] args) {
+
+        ArrayList list01 = new ArrayList();
+        list01.add("LTP");
+        list01.add("Web");
+        list01.add("Algorithms");
+        list01.forEach(l -> System.out.println(l));
+        System.out.println("---------------------");
+
+        ArrayList list02 = new ArrayList();
+        list02.add("LTP");
+        list02.add("Web");
+        list02.set(1,"Algorithms");
+        list02.forEach(l -> System.out.println(l));
+        System.out.println("---------------------");
+
+        ArrayList list03 = new ArrayList();
+        list03.add("LTP");
+        list03.add("Web");
+        list03.add("Algorithms");
+        list03.remove(1);
+        list03.forEach(l -> System.out.println(l));
+        System.out.println("---------------------");
+
+        ArrayList list04 = new ArrayList();
+        list04.add("LTP");
+        list04.add("Web");
+        list04.add("Algorithms");
+        System.out.println(list04.get(1));
+        System.out.println("---------------------");
+
+        ArrayList list05 = new ArrayList();
+        list05.add("LTP");
+        list05.add("Web");
+        list05.add("Algorithm");
+
+        for (int i = 0; i < list05.size(); i++) {
+            System.out.println(list05.get(i));
+        }
+        System.out.println("---------------------");
+
+        ArrayList list06 = new ArrayList();
+        list06.add("LTP");
+        list06.add("Web");
+        list06.add("Algorithm");
+
+        int index = list06.indexOf("Web");
+        System.out.println("The value \"Web\" is at position: " + index);
+
+        System.out.println("---------------------");
+
+        HashSet courses = new HashSet<>();
+
+        courses.add("Java");
+        courses.add(".NET");
+        courses.add("Android");
+
+        courses.add("Java");
+
+        System.out.println(courses);
+
+        System.out.println("---------------------");
+
+        HashMap map = new HashMap();
+
+        map.put("RM1234", "Charles");
+        map.put("RM4321", "Ophelia");
+        map.put("RM5678", "Balthazar");
+
+        System.out.println(map);
+        System.out.println(map.get("RM1234"));
+
+        map.remove("RM1234");
+
+        System.out.println(map.get("RM1234"));
+    }
+}
